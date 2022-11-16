67b53cbb72411edd5c348bf5dfc0bf92cee2e448
João Victor <91824266+jvmg96@users.noreply.github.com>
2022-11-15 22:44:55 -0300

    Codigo

--- sparkcode/votos_codigo.py
+++ sparkcode/votos_codigo.py
@@ -1,36 +1,33 @@
 from pyspark.sql import functions as f
 from pyspark.sql import SparkSession
-
-spark = (
-    SparkSession.builder
-    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.1.0")
-    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
-    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
-    .getOrCreate()
-)
-
-spark.sparkContext.setLogLevel("WARN")
-
-from delta.tables import *
+spark = SparkSession.builder.appName('PySpark_Trabalho_Final').getOrCreate()
 
 print("Reading CSV file from S3")
-schema1 = "sg_uf string, qt_votos_brancos int, dt_carga string"
-votos_brancos = spark.read.csv("s3://jvmg-puc-002112264889/votos_brancos/", header=True, schema=schema1, sep=";")
-ind_brancos = votos_brancos.select('sg_uf','qt_votos_brancos')
-schema2 = "sg_uf string, qt_votos_nom_validos int, dt_carga string"
-votos_validos = spark.read.csv("s3://jvmg-puc-002112264889/votos_validos/", header=True, schema=schema2, sep=";")
-ind_validos = votos_validos.select('sg_uf','qt_votos_nom_validos')
+schema = "sg_uf string, qt_votos_validos int, qt_votos_brancos int, dt_carga string"
+votos = spark.read.csv("s3://jvmg-puc-002112264889/votos/", header=True, schema=schema, sep=";", encoding="latin1")
 
 print("Writing dataset as a parquet table")
-ind_brancos.write.format("parquet").mode("overwrite").save("s3://jvmg-puc-002112264889/parquet_brancos/")
-ind_validos.write.format("parquet").mode("overwrite").save("s3://jvmg-puc-002112264889/parquet_validos/")
+votos.write.format("parquet").mode("overwrite").save("s3://jvmg-puc-002112264889/parquet/")
 
-print("Votos Brancos")
-parquet_brancos = spark.read.parquet("s3://jvmg-puc-002112264889/parquet_brancos/").show()
+print("Votos")
+parquet_votos = spark.read.parquet("s3://jvmg-puc-002112264889/parquet/")
+ind = parquet_votos.select('sg_uf','qt_votos_validos','qt_votos_brancos')
+ind.show()
+print(ind)
 
-print("Votos Válidos")
-parquet_validos = spark.read.parquet("s3://jvmg-puc-002112264889/parquet_validos/").show()
+print("Soma dos Votos")
+soma = ind.withColumn('soma_votos', f.expr("qt_votos_validos + qt_votos_brancos"))
+soma.show()
+print(soma)
+
+print("Subtracao dos Votos")
+subtracao = ind.withColumn('subtracao_votos', f.expr("qt_votos_validos - qt_votos_brancos"))
+subtracao.show()
+print(subtracao)
 
 print("Tabela Final")
-tabela_final = parquet_brancos.merge(parquet_validos, how='inner', on=['sg_uf']).show()
+tabela_final = ind.withColumn('soma_votos', f.expr("qt_votos_validos + qt_votos_brancos"))
+tabela_final = tabela_final.withColumn('subtracao_votos', f.expr("qt_votos_validos - qt_votos_brancos"))
+tabela_final.show()
+print(tabela_final)
 tabela_final.write.mode("overwrite").save("s3://jvmg-puc-002112264889/resultado_final/")
